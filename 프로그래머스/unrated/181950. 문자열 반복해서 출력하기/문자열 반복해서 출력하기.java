9dd421b0e6236bbb6eb00a9534ff59efb8e4436c
괜찮아? <115512549+mydreamis-18@users.noreply.github.com>
2023-09-26 12:36:40 +0900

    [unrated] Title: 문자열 반복해서 출력하기, Time: 153.60 ms, Memory: 67 MB -BaekjoonHub

--- 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -6,10 +6,13 @@ public class Solution {
         String str = sc.next();
         int n = sc.nextInt();
         
-        // ㅜ str과 n를 왜 못 곱해?
+        // // ㅜ str과 n를 왜 못 곱해?
+        // // bad operand types for binary operator '*'
+        // // first type:  String
+        // // second type: int
         // System.out.print(str * n);
         
-        // ㅜ 방법1
+        // // ㅜ 방법1
         // for (int i = 0; i < n; i++) {
         //     System.out.print(str);
         // }
