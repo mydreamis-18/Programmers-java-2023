e07b5d6108de6175810c2e831de2643f513f0d2f
괜찮아? <115512549+mydreamis-18@users.noreply.github.com>
2023-09-29 01:01:23 +0900

    [unrated] Title: 두 수의 연산값 비교하기, Time: 0.04 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    //
+    public int calculation(int a, int b) {
+        //
+        // // ㅜ 오류: 호환되지 않는 유형: double에서 int로 손실이 있는 변환 가능
+        // // error: incompatible types: possible lossy conversion from double to int
+        // return a * Math.pow(10, Integer.toString(b).length()) + b;
+        
+        return a * (int) Math.pow(10, Integer.toString(b).length()) + b;
+    }
+    
+    public int solution(int a, int b) {
+        //
+        // ㅜ 방법1
+        int calculationAb = calculation(a, b);
+        int doubleAb = 2 * a * b;
+        
+        return calculationAb >= doubleAb ? calculationAb : doubleAb;
+    }
+}
