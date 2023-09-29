851e384634f9f1c55104654582a8bd8a11775216
괜찮아? <115512549+mydreamis-18@users.noreply.github.com>
2023-09-30 00:37:17 +0900

    [unrated] Title: 공배수, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.java
+++ 프로그래머스/unrated/181936. 공배수/공배수.java
@@ -1,6 +1,11 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        int answer = number % n == 0 ? number % m == 0 ? 1 : 0 : 0;
+        //
+        // // ㅜ 방법1
+        // int answer = number % n == 0 ? number % m == 0 ? 1 : 0 : 0;
+        
+        // ㅜ 방법2
+        int answer = number % n == 0 && number % m == 0 ? 1 : 0;
         return answer;
     }
 }
