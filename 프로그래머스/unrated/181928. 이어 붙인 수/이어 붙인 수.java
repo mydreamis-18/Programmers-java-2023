e3e368648e179dc0ab1e59ea6a7f8092ddbbf68a
괜찮아? <115512549+mydreamis-18@users.noreply.github.com>
2023-09-30 02:47:10 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,19 +1,35 @@
 class Solution {
     public int solution(int[] num_list) {
         //
-        String odd = "";
-        String even = "";
+//         // ㅜ 방법1
+//         String odd = "", even = "";
+        
+//         for (int num : num_list) {
+//             if (num % 2 == 0) {
+//                 even += num;
+//             }
+//             else {
+//                 odd += num;
+//             }
+//         }
+        
+//         int answer = Integer.parseInt(odd) + Integer.parseInt(even);
+        
+        // ㅜ 방법2
+        int odd = 0, even = 0;
         
         for (int num : num_list) {
             if (num % 2 == 0) {
-                even += Integer.toString(num);
+                even *= 10;
+                even += num;
             }
             else {
-                odd += Integer.toString(num);
+                odd *= 10;
+                odd += num;
             }
         }
         
-        int answer = Integer.parseInt(odd) + Integer.parseInt(even);
+        int answer = odd + even;
         return answer;
     }
 }
